b982684b5e5c0a6b89e400d0af3b6e9b648c5d2d
junio <junioresende23@gmail.com>
2022-04-10 14:56:54 -0300

    formatacao de strings

---
+++ Aulas/Session2/Aula7/aula7.py
@@ -0,0 +1,9 @@
+nome = 'Junio'
+idade = 31
+altura = 1.88
+e_maior = idade > 18
+peso = 140
+imc = peso / (altura ** 2)
+
+print(nome, 'tem', idade, 'anos de idade e seu imc é', imc)
+print(f'{nome} tem {idade} anos de idade e seu imc é {imc:.2f}')
