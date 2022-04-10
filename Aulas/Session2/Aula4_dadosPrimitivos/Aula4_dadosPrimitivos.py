55024e19420f7a6525e218a7b4e96738316f902e
junio <junioresende23@gmail.com>
2022-04-10 14:46:41 -0300

    aula 4 tipos primitivos

---
+++ Aulas/Session2/Aula4_dadosPrimitivos/Aula4_dadosPrimitivos.py
@@ -0,0 +1,11 @@
+"""
+Tipos de dados
+str -  string
+int - inteiro
+float - real/ponto flutuante
+bool - booleano/lógico
+"""
+print('junio', type('junio'))
+print(10, type(10))
+print(25.3, type(25.3))
+print(10 == 10, type(10 == 10))
