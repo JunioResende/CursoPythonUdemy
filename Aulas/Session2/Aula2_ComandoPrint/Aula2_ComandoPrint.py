52c3d87427cf976201176b0a39b27ee89a9cb905
junio <junioresende23@gmail.com>
2022-04-10 14:08:09 -0300

    aula 2 comando print

---
+++ Aulas/Session2/Aula2_ComandoPrint/Aula2_ComandoPrint.py
@@ -0,0 +1,10 @@
+"""
+print(123456)
+print('Junio', 'Resende', sep='-', end=' ')
+print('Isa', 'Paula', sep='-', end=' ')
+"""
+"""
+037.244.971-99
+"""
+print('037', '244', '971', sep='.', end='-')
+print('99')
